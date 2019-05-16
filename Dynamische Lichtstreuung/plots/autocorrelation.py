0ab917d55be2eaecfdee0a1217a6c32db7f8d6ab
David Hoffmann <davarocker@gmail.com>
2019-05-16 16:59:17 +0200

    autocorrelation.py done

--- Dynamische Lichtstreuung/plots/autocorrelation.py
+++ Dynamische Lichtstreuung/plots/autocorrelation.py
@@ -24,26 +24,20 @@ files = ["../data/Ouzo5_Wasser10_10grad-new.txt",
 "../data/Ouzo5_Wasser5_35grad-new.txt",
 "../data/Ouzo5_Wasser5_40grad-new.txt",
 "../data/Ouzo5_Wasser5_5grad_1-new.txt"]
-#for file in files:
-file=files[0]
-print(file)
-Zeit,Kanal1 = np.loadtxt(file,delimiter="\t",skiprows=3,unpack=True)
-N = len(Zeit)
-print(N)
-T = Zeit[1]-Zeit[0]
-print(T)
-Time = np.arange(0,N*T,T)
-print(Time)
-x = Zeit
-y = Kanal1
-yun = y - np.mean(y)
-print(yun)
-ynorm = np.sum(yun**2)
-print(ynorm)
-Auto = np.correlate(yun,yun,"full")/ynorm
-print(Auto)
-#Auto = Auto[len(Auto)/2:]
-#plt.plot(Time,Auto)
-#plt.xscale(log)
-#plt.show()
-np.savetxt(file + "autocorrelation", (Time,Auto),header='Zeit [s], Autokorrelation')
+for file in files:
+#file=files[0]
+    print(file)
+    Zeit,Kanal1 = np.loadtxt(file,delimiter="\t",skiprows=3,unpack=True)
+    N = len(Zeit)
+    T = Zeit[1]-Zeit[0]
+    Time = np.arange(0,N*T,T)
+    x = Zeit
+    y = Kanal1
+    yun = y - np.mean(y)
+    ynorm = np.sum(yun**2)
+    Auto = np.correlate(yun,yun,"full")/ynorm
+    Auto = Auto[int(len(Auto)/2):]
+    #plt.plot(Time,Auto)
+    #plt.xscale(log)
+    #plt.show()
+    np.savetxt("Auto" + file,np.c_[Time,Auto],header='Zeit [s], Autokorrelation')
